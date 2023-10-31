e6c7cf8f3d1dfa6d88792c484d08eb811350dc97
hitashreeD <120547008+hitashreeD@users.noreply.github.com>
2023-10-31 16:10:48 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,8 @@
+class Hello{  
+    public static void main(String[] args){  
+    System.out.println("This is java class for practising jenkins "); 
+    System.out.println("This is java class by using Jenkins");
+    System.out.println("This is java class 2nd build");  
+    System.out.println("This is java class new build");   
+    }  
+    }  
